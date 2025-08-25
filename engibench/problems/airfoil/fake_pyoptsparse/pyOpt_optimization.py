e8788d9c7ffb27685917a49ef3b6e11e571cc152
Gerhard Bräunlich <gerhard.braeunlich@id.ethz.ch>
2025-08-25 11:31:07 +0200

    airfoil: Allow unpickling pyoptsparse if pyoptsparse is not available

---
+++ engibench/problems/airfoil/fake_pyoptsparse/pyOpt_optimization.py
@@ -0,0 +1,6 @@
+# noqa: N999
+"""Drop-in."""
+
+from engibench.problems.airfoil.fake_pyoptsparse import FakePyOptSparseObject as Optimization
+
+__all__ = ["Optimization"]
---
+++ engibench/problems/airfoil/fake_pyoptsparse/pyOpt_variable.py
@@ -0,0 +1,6 @@
+# noqa: N999
+"""Drop-in."""
+
+from engibench.problems.airfoil.fake_pyoptsparse import FakePyOptSparseObject as Variable
+
+__all__ = ["Variable"]
